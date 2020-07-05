6164e292fc096ca3aa54d56d33a1e37e320a83e9
Hermes <abpalema@gmail.com>
2020-07-05 22:31:21 +0200

    working on select buttons

--- gui/main.py
+++ gui/main.py
@@ -14,6 +14,13 @@ matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from functools import partial
+from tkinter.filedialog import askopenfilename
+import csv
+import mysql.connector
+
+
+
+
 
 df = pd.read_csv("../barbershop.csv")
 
@@ -25,8 +32,10 @@ class OtherFrame(tk.Toplevel):
         self.title("Mining")
         self.fig = plt.figure(figsize=(5, 4))
         self.canvas = FigureCanvasTkAgg(self.fig, self)
-        self.calc_set = []
-
+        self.calc_set = set()
+        self.buttonDict = {}
+        self.x_variable = None
+        self.y_variable = None
 
         self.create_widgets()
         sns.set(style="whitegrid")
@@ -47,65 +56,42 @@ class OtherFrame(tk.Toplevel):
         tk.Button(self, text="Box Plot",  command=self.m_box).grid(row=6, columnspan=1)
         tk.Button(self, text="Scatter Plot",  command=self.m_scatter).grid(row=7, columnspan=1)
         tk.Button(self, text="Association").grid(row=8, columnspan=1)
-        tk.Button(self, text="Regression", command=self.m_regression).grid(row=9, columnspan=1)
 
-        count = 0
-        row = 10
+        tk.Label(self, text="x").grid(row=10, column=2)
+        tk.Label(self, text="y").grid(row=10, column=4)
+
+        tk.OptionMenu(self, "select", self.calc_set).grid(row=10, column=3)
+        tk.Entry(self, background="white", fg="black").grid(row=10, column=5)
+
+
+        colc = 8
+        rowc = 0
         for column in df.columns.values:
-            if(count > 7):
-                row = 11
-
-
-            tk.Button(self, text=column, command=partial(self.add_to_set, column)).grid(row=row, column=count+2, columnspan=1)
-            count += 1
+            self.buttonDict[column]=tk.Button(self, text=column, command=partial(self.add_to_set, column))
+            self.buttonDict[column].grid(row=rowc, column=colc, columnspan=1)
+            if(colc == 9):
+                colc -= 1
+                rowc += 1
+            else:
+                colc += 1
+                
 
     def add_to_set(self, column):
-        
-        self.calc_set.append(column)
-        print(self.calc_set)
-
+        if(self.buttonDict[column].cget('bg') == 'green'):
+            self.calc_set.remove(column)
+            self.buttonDict[column].config(background='#383838')
+        else:
+            self.calc_set.add(column)
+            self.buttonDict[column].config(background='green')
         return None
 
 
-    def m_regression(self):
-        self.fig.clf()
-        sns.lmplot(x="clients_per_month", y="monthly_income", hue="shop_type", data=df, markers=["o", "x"])
-        self.draw_canvas()
-
-    # def m_apryori(self):
-    #     self.fig.clf()
-    #     records = []
-    #     for i in range(0, 50):
-    #         records.append([str(df.values[i,j]) for j in range(5, 6)])
-    #         association_rules = apriori(records, min_support=0.0045, min_confidence=0.2, min_lift=3, min_length=2)
-    #         association_results = list(association_rules)
-    #         print(len(association_rules))
-    #         print(association_rules[0])
-        
-    #     for item in association_rules:
-
-    #         # first index of the inner list
-    #         # Contains base item and add item
-    #         pair = item[0] 
-    #         items = [x for x in pair]
-    #         print("Rule: " + items[0] + " -> " + items[1])
-
-    #         #second index of the inner list
-    #         print("Support: " + str(item[1]))
-
-    #         #third index of the list located at 0th
-    #         #of the third index of the inner list
-
-    #         print("Confidence: " + str(item[2][0][2]))
-    #         print("Lift: " + str(item[2][0][3]))
-    #         print("=====================================")
-
     def m_scatter(self):
         self.fig.clf()
-        plt.scatter(df.clients_per_month, df.monthly_income)
+        plt.scatter(df[self.y_variable], df[self.x_variable])
         plt.title('Barbershop Scatterplot')
-        plt.xlabel('Monthly Income')
-        plt.ylabel('Clients per Month')
+        plt.xlabel(self.x_variable)
+        plt.ylabel(self.y_variable)
         self.draw_canvas()
 
     def m_cluster(self):
@@ -117,22 +103,21 @@ class OtherFrame(tk.Toplevel):
 
     def m_hist(self):
         self.fig.clf()
-        clients = self.fig.add_subplot(121)
-        income = self.fig.add_subplot(122)
 
-        clients.hist(df.clients_per_month, bins=10)
-        clients.set_xlabel('Count')
-        clients.set_title("Clients per Month")
-
-        income.hist(df.monthly_income, bins=10)
-        income.set_xlabel('N$')
-        income.set_title("Monthly Barber Income")
+        for (i, itm) in enumerate(self.calc_set):
+            clients = self.fig.add_subplot(12+i+1)
+            clients.hist(df[itm], bins=10)
+            clients.set_xlabel('Count')
+            clients.set_title(itm + " histogram")
+            
         self.draw_canvas()
+
+        
 
 
     def m_box(self):
         self.fig.clf()
-        sns.boxplot(x='shop_type', y='cost', data=df, orient="v")
+        sns.boxplot(x=self.x_variable, y=self.y_variable, data=df, orient="v")
         self.draw_canvas()
 
     def m_pie(self):
@@ -144,7 +129,7 @@ class OtherFrame(tk.Toplevel):
 
     def m_bar(self):
         self.fig.clf()
-        sns.countplot(x='hairstyle', data=df[self.calc_set])
+        sns.countplot(x=self.x_variable, data=df[self.calc_set])
         self.draw_canvas()
 
     def draw_canvas(self):
@@ -159,10 +144,38 @@ class MyApp(object):
         self.frame = tk.Frame(parent)
         self.frame.grid()
 
-        btn = tk.Button(self.frame, text="mine", command=self.openFrame)
-        btn.grid(row=7, column=2)
+        tk.Button(self.frame, text="mine", command=self.openFrame).grid(row=0, column=3)
+        tk.Button(self.frame ,text="Submit").grid(row=11,column=3)
+        tk.Button(self.frame, text="Upload", command=self.upload).grid(row=1, column=3, pady=10)
+        self.selectField = tk.Entry(self.frame, text="mine")
+        self.selectField.grid(row=1, column=0, columnspan=3, pady=10)
+        self.selectField.bind("<Double-Button-1>", self.someFile)        
 
         self.create_widgets()
+
+    def upload(self):
+        mydb = mysql.connector.connect(host='localhost',
+            user='root',
+            passwd='',
+            db='mydb')
+        cursor = mydb.cursor()
+
+        csv_data = csv.reader(self.filename)
+        for row in csv_data:
+
+            cursor.execute('INSERT INTO testcsv(names, \
+                classes, mark )' \
+                'VALUES("%s", "%s", "%s")', 
+                row)
+        #close the connection to the database.
+        cursor.close()
+        return 0
+
+    def someFile(self, event):
+        self.filename = askopenfilename(filetypes = (("CSV files","*.csv"), ("all files", "*.*")))
+        self.selectField.delete(1,"end")
+        self.selectField.insert(1, self.filename)
+        return None
 
     def hide(self):
         self.root.withdraw()
@@ -178,52 +191,53 @@ class MyApp(object):
     
     def create_widgets(self):
 
-        tk.Label(self.frame, text="Cellphone").grid(row=0)
-        tk.Label(self.frame, text="Style").grid(row=0, column=2)
-        tk.Label(self.frame, text="Price").grid(row=1)
-        tk.Label(self.frame, text="Rent").grid(row=1, column=2)
-        tk.Label(self.frame, text="Transport").grid(row=2)
-        tk.Label(self.frame, text="Clients").grid(row=2, column=2)
-        tk.Label(self.frame, text="Income").grid(row=3)
-        tk.Label(self.frame, text="Transport Type").grid(row=3, column=2)
-        tk.Label(self.frame, text="Hours").grid(row=4)
-        tk.Label(self.frame, text="Trip").grid(row=4, column=2)
-        tk.Label(self.frame, text="Shop").grid(row=5)
-        tk.Label(self.frame, text="Plat1").grid(row=5, column=2)
-        tk.Label(self.frame, text="Plat2").grid(row=6)
 
-        self.cell = tk.Entry(self.frame)
-        self.style = tk.Entry(self.frame)
-        self.cost = tk.Entry(self.frame)
-        self.rent = tk.Entry(self.frame)
-        self.transport = tk.Entry(self.frame)
-        self.clients = tk.Entry(self.frame)
-        self.income = tk.Entry(self.frame)
-        self.tform = tk.Entry(self.frame)
-        self.hours = tk.Entry(self.frame)
-        self.trip = tk.Entry(self.frame)
-        self.shop = tk.Entry(self.frame)
-        self.plat1 = tk.Entry(self.frame)
-        self.plat2 = tk.Entry(self.frame)
+        tk.Label(self.frame, text="Barbershop Form").grid(row=3, columnspan=3, pady=15)
+        tk.Label(self.frame, text="Cellphone").grid(row=4)
+        tk.Label(self.frame, text="Style").grid(row=4, column=2)
+        tk.Label(self.frame, text="Price").grid(row=5)
+        tk.Label(self.frame, text="Rent").grid(row=5, column=2)
+        tk.Label(self.frame, text="Transport").grid(row=6)
+        tk.Label(self.frame, text="Clients").grid(row=6, column=2)
+        tk.Label(self.frame, text="Income").grid(row=7)
+        tk.Label(self.frame, text="Transport Type").grid(row=7, column=2)
+        tk.Label(self.frame, text="Hours").grid(row=8)
+        tk.Label(self.frame, text="Trip").grid(row=8, column=2)
+        tk.Label(self.frame, text="Shop").grid(row=9)
+        tk.Label(self.frame, text="Plat1").grid(row=9, column=2)
+        tk.Label(self.frame, text="Plat2").grid(row=10)
 
-        self.cell.grid(row=0, column=1)
-        self.style.grid(row=0, column=3)
-        self.cost.grid(row=1, column=1) 
-        self.rent.grid(row=1, column=3) 
-        self.transport.grid(row=2, column=1) 
-        self.clients.grid(row=2, column=3) 
-        self.income.grid(row=3, column=1)
-        self.tform.grid(row=3, column=3)
-        self.hours.grid(row=4, column=1)
-        self.trip.grid(row=4, column=3) 
-        self.shop.grid(row=5, column=1) 
-        self.plat1.grid(row=5, column=3)
-        self.plat2.grid(row=6, column=1)
+        self.cell = tk.Entry(self.frame, background="white", fg="black")
+        self.style = tk.Entry(self.frame, background="white", fg="black")
+        self.cost = tk.Entry(self.frame, background="white", fg="black")
+        self.rent = tk.Entry(self.frame, background="white", fg="black")
+        self.transport = tk.Entry(self.frame, background="white", fg="black")
+        self.clients = tk.Entry(self.frame, background="white", fg="black")
+        self.income = tk.Entry(self.frame, background="white", fg="black")
+        self.tform = tk.Entry(self.frame, background="white", fg="black")
+        self.hours = tk.Entry(self.frame, background="white", fg="black")
+        self.trip = tk.Entry(self.frame, background="white", fg="black")
+        self.shop = tk.Entry(self.frame, background="white", fg="black")
+        self.plat1 = tk.Entry(self.frame, background="white", fg="black")
+        self.plat2 = tk.Entry(self.frame, background="white", fg="black")
+
+        self.cell.grid(row=4, column=1)
+        self.style.grid(row=4, column=3)
+        self.cost.grid(row=5, column=1) 
+        self.rent.grid(row=5, column=3) 
+        self.transport.grid(row=6, column=1) 
+        self.clients.grid(row=6, column=3) 
+        self.income.grid(row=7, column=1)
+        self.tform.grid(row=7, column=3)
+        self.hours.grid(row=8, column=1)
+        self.trip.grid(row=8, column=3) 
+        self.shop.grid(row=9, column=1) 
+        self.plat1.grid(row=9, column=3)
+        self.plat2.grid(row=10, column=1)
 
         
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.geometry("500x400")
-    app = MyApp(root)
-    root.mainloop()
+root = tk.Tk()
+root.geometry("500x400")
+app = MyApp(root)
+root.mainloop()
